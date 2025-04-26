0ec8edc3120246843786216ebbacbca9b16cc338
Thecabezon <jeffersonrojassosa150@gmail.com>
2025-04-26 12:45:43 -0500

    feat: interfaz de teclado funcional y corrección de estilos

--- app/src/main/java/com/rojas/jefferson/laboratoriocalificado01/Ejercicio01.kt
+++ app/src/main/java/com/rojas/jefferson/laboratoriocalificado01/Ejercicio01.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.content.Intent
 
 class Ejercicio01 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,13 @@ class Ejercicio01 : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        // Encuentra el botón por su ID
+        val btnIrTeclado = findViewById<Button>(R.id.btnIrTeclado)
+        btnIrTeclado.setOnClickListener {
+            // Crea el Intent para ir a Ejercicio02
+            val intent = Intent(this, Ejercicio02::class.java)
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/com/rojas/jefferson/laboratoriocalificado01/Ejercicio02.kt
@@ -0,0 +1,16 @@
+package com.rojas.jefferson.laboratoriocalificado01
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Ejercicio02 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_ejercicio02)
+
+    }
+}
